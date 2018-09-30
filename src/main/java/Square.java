13c55bd378de30d7a6112548f5a297ff40f5f920
Evgeny <p37r0ff1@yandex.ru>
2018-09-30 18:37:53 +0300

    6.0 Square

---
+++ src/main/java/Square.java
@@ -0,0 +1,20 @@
+/**
+ * Class 6.0 от 1 до n возведенными в квадрат
+ * @author semenov
+ * @since 18.09.2018
+ * @version 1.0
+ */
+public class Square {
+    /**
+     * Метод принимающий положительное целое число n
+     * @param bound положительное целое число, ширина
+     * @return возвращает заполненый массив от 1 до n возведенными в квадрат
+     */
+    public int[] calculate(int bound) {
+        int[] rst = new int[bound];
+        for (int i = 1; i < bound + 1; i++) {
+            rst[i - 1] = i * i;
+        }
+        return rst;
+    }
+}
