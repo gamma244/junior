4b750758ba122f227d212ccc97b655b7a3d62151
Evgen <p37r0ff1@yandex.ru>
2018-10-05 21:40:24 +0300

    Revert "plusminus3"
    This reverts commit 7e59a65

--- src/main/java/Square.java
+++ src/main/java/Square.java
@@ -15,7 +15,7 @@ public class Square {
         for (int i = 1; i < bound + 1; i++) {
             rst[i - 1] = i * i;
         }
-        int plusminus = 1 + 1 - 5;
+        int plusminus2 = 1 + 1 - 5;
         return rst;
     }
 }
