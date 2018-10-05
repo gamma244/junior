f01d6792e6f54abdb78c3e747f59b13b42c28507
Evgen <p37r0ff1@yandex.ru>
2018-10-05 23:53:48 +0300

    add 3

--- src/main/java/Square.java
+++ src/main/java/Square.java
@@ -15,7 +15,7 @@ public class Square {
         for (int i = 1; i < bound + 1; i++) {
             rst[i - 1] = i * i;
         }
-        int summ2 = 1 + 1 - 5;
+        int summ3 = 1 + 1 - 5;
         return rst;
     }
 }
